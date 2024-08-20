e8c97d09c18bd6e6fd1e7b9bdafaa4f6545339cc
NhaPhuowng <phuongptn03@gmail.com>
2024-08-20 16:48:39 +0700

    commit

---
+++ src/main/java/com/hieu/demojpa/service/Impl/ProductService.java
@@ -0,0 +1,8 @@
+package com.hieu.demojpa.service.Impl;
+
+import com.hieu.demojpa.service.IProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService implements IProductService {
+}
